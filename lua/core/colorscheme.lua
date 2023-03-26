25e2b6a3ddb81bcb2a5a4fa15dca166c0a4029c7
Dadatoa <dadatoa.webdev@outlook.fr>
2023-03-26 20:28:52 +0700

    change colorscheme

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,7 +1,7 @@
 -- set colorscheme to nightfly with protected call
 -- in case it isn't installed
 -- colorscheme solarized
-local status, _ = pcall(vim.cmd, "colorscheme nightfly")
+local status, _ = pcall(vim.cmd, "colorscheme solarized")
 if not status then
 	print("Colorscheme not found!") -- print error if colorscheme not installed
 	return
